ee1a525e044f861491756ef590d2790a83a9e921
Misaka <misaka1033@gmail.com>
2023-02-14 18:17:45 +0800

    u

--- global/global.go
+++ global/global.go
@@ -3,6 +3,7 @@ package global
 import (
 	"server/config"
 
+	"github.com/songzhibin97/gkit/cache/local_cache"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 )
@@ -11,4 +12,6 @@ var (
 	GGG_VP     *viper.Viper // 全局Viper
 	GGG_LOG    *zap.Logger
 	GGG_CONFIG config.Server // 存放从yaml配置文件读取的结构体
+
+	BlackCache local_cache.Cache
 )
--- initialize/outer.go
+++ initialize/outer.go
@@ -1,4 +1,25 @@
 package initialize
 
+import (
+	"fmt"
+	"server/global"
+	"server/utils"
+
+	"github.com/songzhibin97/gkit/cache/local_cache"
+)
+
 func OtherInit() {
+	dr, err := utils.ParseDuration(global.GGG_CONFIG.JWT.ExpiresTime)
+	if err != nil {
+		panic(err)
+	}
+	_, err = utils.ParseDuration(global.GGG_CONFIG.JWT.BufferTime)
+	if err != nil {
+		panic(err)
+	}
+	global.BlackCache = local_cache.NewCache(
+		local_cache.SetDefaultExpire(dr),
+	)
+	fmt.Println("BlackCache ", global.BlackCache)
+
 }
